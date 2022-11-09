449a2793a4dde983a10872126993dc6a16d057ba
mijo <mojahidbelaman@gmail.com>
2022-11-09 11:58:32 +0100

    post data to server and change in UI

--- book-client/src/store/bookSlice.js
+++ book-client/src/store/bookSlice.js
@@ -1,28 +1,53 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 import axios from 'axios'
 
-export const fetchBooks = createAsyncThunk("book/fetchBooks", async () => {
+export const fetchBooks = createAsyncThunk("book/fetchBooks", async (args, thunkAPI) => {
+    const {rejectWithValue} = thunkAPI
     try {
         const response = await axios.get("http://localhost:3005/books");
         return response.data;
     } catch (error) {
-        console.log(error);
+        return rejectWithValue(error);
+    }
+})
+
+export const addBook = createAsyncThunk("book/addBook", async (data, thunkAPI) => {
+    const {rejectWithValue} = thunkAPI
+    try {
+        await axios.post("http://localhost:3005/books", data);
+    } catch (error) {
+        return rejectWithValue(error);
     }
 })
 
 const bookSlice = createSlice({
     name: 'book',
-    initialState: {books: [], isLoading: false, error: null},
+    initialState: {data: [], isLoading: false, error: null},
     extraReducers: {
+        // get all books
         [fetchBooks.pending]: (state, action) => {
             state.isLoading = true
             state.error = null
         },
         [fetchBooks.fulfilled]: (state, action) => {
             state.isLoading = false
-            state.books = action.payload
+            state.data = action.payload
         },
         [fetchBooks.rejected]: (state, action) => {
+            state.isLoading = false
+            state.error = action.payload
+        },
+        // add book
+        [addBook.pending]: (state, action) => {
+            state.isLoading = true
+            state.error = null
+        },
+        [addBook.fulfilled]: (state, action) => {
+            console.log(action);
+            state.isLoading = false
+            state.data.push(action.meta.arg)
+        },
+        [addBook.rejected]: (state, action) => {
             state.isLoading = false
             state.error = action.payload
         }
