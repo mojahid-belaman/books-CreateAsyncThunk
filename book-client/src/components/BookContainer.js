a9365d3f56fb9b5857f79420759f6380715b6dcf
mijo <mojahidbelaman@gmail.com>
2022-11-09 18:17:10 +0100

    add function get book and display in component

--- book-client/src/components/BookContainer.js
+++ book-client/src/components/BookContainer.js
@@ -8,18 +8,24 @@ import Error from "./Error";
 const BookContainer = () => {
   const dispatch = useDispatch();
   const { data, isLoading, error } = useSelector((state) => state.books);
+  const [book, setBook] = React.useState(null);
 
   useEffect(() => {
     dispatch(fetchBooks());
   }, [dispatch]);
+
+  const getInfoBook = (id) => {
+    const book = data?.find((book) => book.id === id);
+    setBook(book);
+  }
   return (
     <>
       {error ? (
         <Error text={"Server Is Down!."} />
       ) : (
         <div className="flex gap-5">
-          <BookList books={data} isLoading={isLoading} />
-          <BookInfo />
+          <BookList books={data} isLoading={isLoading} getInfoBook={getInfoBook} dispatch={dispatch} />
+          <BookInfo book={book} />
         </div>
       )}
     </>
